5ead41838d1b93fe2e1cbde731ae255caa3b01a5
Muhannad Ashraf <mu7nnd.as@gmail.com>
2025-09-16 20:13:23 +0300

    Add CUDA kernel for shared memory optimized softmax implementation

--- Softmax/kernels/shared_kernel.cu
+++ Softmax/kernels/shared_kernel.cu
@@ -4,14 +4,14 @@
 
 #include "utils.cu"
 
-__global__ void shared_softmax(float* inp, float* out, int N){
+__global__ void shared_softmax(float* inp, float* out, int M, int N){
 
     __shared__ float shared_mem[1024];
 
     int tid = threadIdx.x;
     int row = blockIdx.x;
 
-    if (row >= N) return;
+    if (row >= M) return;
 
     float* inp_row = inp + row * N;
     float* out_row = out + row * N;
@@ -40,6 +40,9 @@ __global__ void shared_softmax(float* inp, float* out, int N){
     }
     float global_max = shared_mem[0];
     __syncthreads();
+    
+    shared_mem[tid] = norm_val * expf(local_max- global_max);
+    __syncthreads();
 
     for(int stride = blockDim.x/2; stride > 0; stride >>=1){
         if(tid < stride){
@@ -53,6 +56,24 @@ __global__ void shared_softmax(float* inp, float* out, int N){
     for (int i = tid; i < N; i += blockDim.x) {
         out_row[i] = expf(inp_row[i] - global_max) / global_norm;
     }
-    
-    
+}
+
+void run_shuffled_softmax(float* inp, float* out, int M, int N){
+    dim3 ThreadsPerBlock(1024);
+    dim3 BlocksPerGrid(M);
+
+    cudaEvent_t start, stop;
+    CUDA_CHECK(cudaEventCreate(&start));
+    CUDA_CHECK(cudaEventCreate(&stop));
+    float ms = 0.f;
+
+    CUDA_CHECK(cudaEventRecord(start));
+    shared_softmax<<<BlocksPerGrid, ThreadsPerBlock>>>(inp, out, M, N);
+    CUDA_CHECK(cudaEventRecord(stop));
+    CUDA_CHECK(cudaEventSynchronize(stop));
+    CUDA_CHECK(cudaEventElapsedTime(&ms, start, stop));
+    printf(">> Kernel execution time: %f ms\n", ms);
+
+    CUDA_CHECK(cudaEventDestroy(start));
+    CUDA_CHECK(cudaEventDestroy(stop));
 }
